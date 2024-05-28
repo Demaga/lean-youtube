c7f510b0c9a74fb22ee5e9f1103afb1580317bfd
nikitius20 <zaika.nikita.01@gmail.com>
2024-05-28 18:42:55 +0300

    Validation for inputs

--- settings.js
+++ settings.js
@@ -71,16 +71,30 @@ let min_duration_output = document.getElementById("min_duration_output");
 browser.storage.local.get("min_duration").then((min_duration_val) => {
     console.log(min_duration_val);
     if (!min_duration_val == false) {
-        browser.storage.local.set({ "min_duration": 61 });
+        browser.storage.local.set({ "min_duration": 60 });
     };
     min_duration.value = min_duration_val.min_duration;
     min_duration_output.innerHTML = min_duration_val.min_duration;
 })
 min_duration.addEventListener("input", function () {
-    browser.storage.local.set({ "min_duration": this.value }).then(() => {
-        console.log("min_duration set to", this.value);
-        min_duration_output.innerHTML = this.value;
-    })
+    if(this.value<60 || this.value>600){
+        //mabe make some allert for invalid numbers
+    }else(
+        browser.storage.local.set({ "min_duration": this.value }).then(() => {
+            console.log("min_duration set to", this.value);
+            min_duration_output.innerHTML = this.value;
+        })
+    )
+    
+});
+min_duration.addEventListener("focusout", function () {
+    if(this.value<60){
+        this.value=60
+        min_duration_output.innerHTML = 60;
+    }else if(this.value>600){
+        this.value=600
+        min_duration_output.innerHTML = 600;
+    }
 });
 
 let max_duration = document.getElementById("max_duration");
@@ -94,8 +108,22 @@ browser.storage.local.get("max_duration").then((max_duration_val) => {
     max_duration_output.innerHTML = max_duration_val.max_duration;
 })
 max_duration.addEventListener("input", function () {
-    browser.storage.local.set({ "max_duration": this.value }).then(() => {
-        console.log("max_duration set to", this.value);
-        max_duration_output.innerHTML = this.value;
-    })
+    if(this.value<601 || this.value>3600){
+        //mabe make some allert for invalid numbers
+    }else(
+        browser.storage.local.set({ "max_duration": this.value }).then(() => {
+            console.log("max_duration set to", this.value);
+            max_duration_output.innerHTML = this.value;
+        })
+    )
+
+});
+max_duration.addEventListener("focusout", function () {
+    if(this.value<601){
+        this.value=601
+        max_duration_output.innerHTML = 601;
+    }else if(this.value>3600){
+        this.value=3600
+        max_duration_output.innerHTML = 3600;
+    }
 });
