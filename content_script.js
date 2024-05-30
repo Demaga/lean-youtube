d1f000733d333e335b984f31eab2d2d18708bf85
nikitius20 <zaika.nikita.01@gmail.com>
2024-05-30 23:02:16 +0300

    section_renderer

--- content_script.js
+++ content_script.js
@@ -1,21 +1,26 @@
-let ytd_guide_renderer  
-if(!!document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)")){
-    ytd_guide_renderer = document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)")
-}else if(!!document.querySelector("ytd-guide-section-renderer>ytd-guide-entry-renderer:nth-child(2)")){
-    ytd_guide_renderer = document.querySelector("ytd-guide-section-renderer>ytd-guide-entry-renderer:nth-child(2)")
-}
-
-
 browser.storage.local.onChanged.addListener(
     (e) => {
         switch(true){
             case "hide_shorts" in e:
+                const ytd_mini_guide_renderer = document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)")
+                const ytd_guide_section_renderer = document.querySelector(".ytd-guide-section-renderer>.ytd-guide-entry-renderer:nth-child(2)") 
+                console.log("ytd_mini_guide_renderer",ytd_mini_guide_renderer)  
+                console.log("ytd_guide_section_renderer",ytd_guide_section_renderer)  
                 if(e.hide_shorts.newValue){
-
-                    ytd_guide_renderer.style.display = "none"
+                    if(ytd_mini_guide_renderer){
+                        ytd_mini_guide_renderer.style.display = "none"
+                    }
+                    if(ytd_guide_section_renderer){
+                        ytd_guide_section_renderer.style.display = "none"
+                    }
                     document.querySelector("ytd-rich-section-renderer").style.display = "none"
                 }else{
-                    ytd_guide_renderer.style.display = "block"
+                    if(ytd_mini_guide_renderer){
+                        ytd_mini_guide_renderer.style.display = "block"
+                    }
+                    if(ytd_guide_section_renderer){
+                        ytd_guide_section_renderer.style.display = "block"
+                    }
                     document.querySelector("ytd-rich-section-renderer").style.display = "block"
                 };
                 break;
