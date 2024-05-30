62058e4e0738e026fd643ffa3f5a8133238b4418
nikitius20 <zaika.nikita.01@gmail.com>
2024-05-30 22:49:52 +0300

    Renderer

--- content_script.js
+++ content_script.js
@@ -1,12 +1,21 @@
+let ytd_guide_renderer  
+if(!!document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)")){
+    ytd_guide_renderer = document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)")
+}else if(!!document.querySelector("ytd-guide-section-renderer>ytd-guide-entry-renderer:nth-child(2)")){
+    ytd_guide_renderer = document.querySelector("ytd-guide-section-renderer>ytd-guide-entry-renderer:nth-child(2)")
+}
+
+
 browser.storage.local.onChanged.addListener(
     (e) => {
         switch(true){
             case "hide_shorts" in e:
                 if(e.hide_shorts.newValue){
-                    document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)").style.display = "none"
+
+                    ytd_guide_renderer.style.display = "none"
                     document.querySelector("ytd-rich-section-renderer").style.display = "none"
                 }else{
-                    document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)").style.display = "block"
+                    ytd_guide_renderer.style.display = "block"
                     document.querySelector("ytd-rich-section-renderer").style.display = "block"
                 };
                 break;
