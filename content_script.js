0c4c75a1b20597b9069752a58193be8176374972
Bohdan Kholodenko <bohdan.kholodenko@gmail.com>
2024-05-31 00:00:22 +0300

    Hide shorts / hide community

--- content_script.js
+++ content_script.js
@@ -1,11 +1,41 @@
+function toggle_shorts(val) {
+    var rich_sections = document.querySelectorAll("ytd-rich-section-renderer");
+
+    rich_sections.forEach((el) => {
+        title = el.querySelector("#title");
+
+        if (title.innerText == 'Shorts') {
+            if (val)
+                el.style.display = 'none'
+            else
+                el.style.display = 'block'
+        }
+    })
+}
+
+
+function toggle_community(val) {
+    var rich_sections = document.querySelectorAll("ytd-rich-section-renderer");
+
+    rich_sections.forEach((el) => {
+        title = el.querySelector("#title");
+
+        if (title.innerText != 'Shorts') {
+            if (val)
+                el.style.display = 'none'
+            else
+                el.style.display = 'block'
+        }
+    })
+}
+
+
 browser.storage.local.onChanged.addListener(
     (e) => {
         switch (true) {
             case "hide_shorts" in e:
                 const ytd_mini_guide_renderer = document.querySelector(".ytd-mini-guide-renderer>ytd-mini-guide-entry-renderer:nth-child(2)")
                 const ytd_guide_section_renderer = document.querySelector("ytd-guide-section-renderer ytd-guide-entry-renderer:nth-child(2)")
-                console.log("ytd_mini_guide_renderer", ytd_mini_guide_renderer)
-                console.log("ytd_guide_section_renderer", ytd_guide_section_renderer)
                 if (e.hide_shorts.newValue) {
                     if (ytd_mini_guide_renderer) {
                         ytd_mini_guide_renderer.style.display = "none"
@@ -13,10 +43,7 @@ browser.storage.local.onChanged.addListener(
                     if (ytd_guide_section_renderer) {
                         ytd_guide_section_renderer.style.display = "none"
                     }
-                    sections = document.querySelectorAll("ytd-rich-section-renderer")
-                    sections.forEach(el => {
-                        el.style.display = "none"
-                    });
+                    toggle_shorts(true)
                 } else {
                     if (ytd_mini_guide_renderer) {
                         ytd_mini_guide_renderer.style.display = "block"
@@ -24,15 +51,14 @@ browser.storage.local.onChanged.addListener(
                     if (ytd_guide_section_renderer) {
                         ytd_guide_section_renderer.style.display = "block"
                     }
-                    sections = document.querySelectorAll("ytd-rich-section-renderer")
-                    sections.forEach(el => {
-                        el.style.display = "block"
-                    });
+                    toggle_shorts(false)
                 };
                 break;
             case "hide_community" in e:
-                break;
-            case "hide_livestream" in e:
+                if (e.hide_community.newValue) {
+                    toggle_community(true)
+                } else
+                    toggle_community(false)
                 break;
         }
     }
